d6d0524999a8d4ee2bf3eab9ef14a49631f1c752
wufangbing <wufangbing@xinhehui.com>
2018-10-26 09:46:15 +0800

    add

---
+++ yjd.sql
@@ -0,0 +1,18 @@
+ALTER TABLE sr_user ADD COLUMN user_type TINYINT NOT NULL DEFAULT 0 COMMENT '0、个人用户 1、企业用户';
+
+DROP TABLE sr_company;
+CREATE TABLE sr_company(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		sruid INT NOT NULL COMMENT 'sr用户ID',
+		legal_person_name VARCHAR(20) COMMENT '法人名称',
+		legal_id_number VARCHAR(18) COMMENT '法人身份证',
+		KEY sr_company_sruid(sruid)
+		)COMMENT '企业信息表'; 
+
+ALTER TABLE sr_order ADD COLUMN actual_loan_amount DECIMAL(19,0) DEFAULT NULL COMMENT '借款金额 单位（分）' AFTER loan_amount;
+UPDATE sr_order SET actual_loan_amount = loan_amount;
+
+
+
+
+
