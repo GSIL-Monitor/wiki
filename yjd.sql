7254162b70f06f626c8692b5864886e39dec7bf6
wufangbing <wufangbing@xinhehui.com>
2018-11-05 15:26:27 +0800

    add

--- yjd.sql
+++ yjd.sql
@@ -1,4 +1,4 @@
-ALTER TABLE sr_user ADD COLUMN user_type TINYINT NOT NULL DEFAULT 0 COMMENT '0、个人用户 1、企业用户';
+ALTER TABLE sr_user ADD COLUMN TYPE INT(2) NOT NULL DEFAULT '0' AFTER email COMMENT '1:个人 2:企业';
 
 DROP TABLE sr_company;
 CREATE TABLE sr_company(
@@ -6,19 +6,16 @@ CREATE TABLE sr_company(
 		sruid INT NOT NULL COMMENT 'sr用户ID',
 		legal_person_name VARCHAR(20) COMMENT '法人名称',
 		legal_id_number VARCHAR(18) COMMENT '法人身份证',
+		ctime INT(11) DEFAULT NULL,
+		mtime INT(11) DEFAULT NULL,
 		KEY idx_sruid(sruid)
 		)COMMENT '企业信息表'; 
 		
 -- 增加实际放款金额
-ALTER TABLE sr_order ADD COLUMN actual_loan_amount DECIMAL(19,0) DEFAULT NULL COMMENT '借款金额 单位（分）' AFTER loan_amount;
-UPDATE sr_order SET actual_loan_amount = loan_amount;
+
+ALTER TABLE sr_order ADD COLUMN lending_amount DECIMAL(19,0) DEFAULT NULL AFTER signing_time COMMENT '放款金额 单位（分）';
+UPDATE sr_offline_repayment SET lending_amount = loan_amount;
 
 
 
 ALTER TABLE sr_product MODIFY COLUMN product_code VARCHAR(20) NOT NULL COMMENT '产品code';
-
-
-
-
-
-
