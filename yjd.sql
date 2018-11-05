d18872249101a4a20211049d0e9281566d68420a
wufangbing <wufangbing@xinhehui.com>
2018-11-05 14:58:02 +0800

    add

--- yjd.sql
+++ yjd.sql
@@ -6,11 +6,17 @@ CREATE TABLE sr_company(
 		sruid INT NOT NULL COMMENT 'sr用户ID',
 		legal_person_name VARCHAR(20) COMMENT '法人名称',
 		legal_id_number VARCHAR(18) COMMENT '法人身份证',
-		KEY sr_company_sruid(sruid)
+		KEY idx_sruid(sruid)
 		)COMMENT '企业信息表'; 
-
+		
+-- 增加实际放款金额
 ALTER TABLE sr_order ADD COLUMN actual_loan_amount DECIMAL(19,0) DEFAULT NULL COMMENT '借款金额 单位（分）' AFTER loan_amount;
 UPDATE sr_order SET actual_loan_amount = loan_amount;
+
+
+
+ALTER TABLE sr_product MODIFY COLUMN product_code VARCHAR(20) NOT NULL COMMENT '产品code';
+
 
 
 
