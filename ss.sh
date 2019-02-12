f0fa0cd5b6ffe5a794acd3f01f701e48799cc757
wufangbing <wufangbing@xinhehui.com>
2019-02-12 14:26:27 +0800

    add

--- ss.sh
+++ ss.sh
@@ -2,5 +2,5 @@ yum -y install epel-release
 yum -y install python-pip
 pip install --upgrade pip
 pip install shadowsocks
-echo -e "{\n\"server\":\"0.0.0.0\",\n"server_port\":26,\n\"password\":\"fankiwu\",\n\"timeout\":600,\n\"method\":\"aes-256-cfb\",\n\"auth\": true\n}" >  /etc/shadowsocks.json
+echo -e "{\n\"server\":\"0.0.0.0\",\n\"server_port\":26,\n\"password\":\"fankiwu\",\n\"timeout\":600,\n\"method\":\"aes-256-cfb\",\n\"auth\": true\n}" >  /etc/shadowsocks.json
 ssserver -c /etc/shadowsocks.json -d start
