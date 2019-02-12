e5107c106e7fd2a1435a5718aa08682bfb4cd5e0
wufangbing <wufangbing@xinhehui.com>
2019-02-12 14:28:47 +0800

    add

--- ss.sh
+++ ss.sh
@@ -4,3 +4,4 @@ pip install --upgrade pip
 pip install shadowsocks
 echo -e "{\n\"server\":\"0.0.0.0\",\n\"server_port\":26,\n\"password\":\"fankiwu\",\n\"timeout\":600,\n\"method\":\"aes-256-cfb\",\n\"auth\": true\n}" >  /etc/shadowsocks.json
 ssserver -c /etc/shadowsocks.json -d start
+systemctl stop firewalld.service
